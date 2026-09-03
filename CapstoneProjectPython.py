f943273dabd3ea6254bf168286a0b568e951fd1f
Derek <dereksun31@gmail.com>
2026-09-03 14:06:16 -0400

    First function made

--- CapstoneProjectPython.py
+++ CapstoneProjectPython.py
@@ -1,1 +1,24 @@
-print("Test, initial day setup.")
+import csv
+import os
+from datetime import datetime
+import matplotlib.pyplot as plt
+FILE_NAME = "transactions.csv"
+
+
+transactions = []
+budget = None
+
+#File functions
+
+def load_transactions():
+    if not os.path.exists(FILE_NAME):
+        return
+
+    with open(FILE_NAME, mode='r', newline="") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            transactions.append({
+                "name": row["name"],
+                "amount": float(row["amount"]),
+                "date": datetime.
+            })
